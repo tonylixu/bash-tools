7a2c1ba166430652f50e15b18de51d49523b1f9e
tonylixu@gmail.com <tonylixu@gmail.com>
2017-12-10 10:38:00 -0500

    Added png file for hardlinks

--- count-size-exclude-hard-links.sh
+++ count-size-exclude-hard-links.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# From "find" man page
+# -links n
+#     File has n links
+# For explanation of hardlinks, checkout the "images/hard-links.png"
 find . -type f -links 1 -exec ls -l {} \;| awk '{s=s+$5} END {print s/(1024*1024) "MB"}'
